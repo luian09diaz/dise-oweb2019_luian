0e8bc7d794259ec88dd459d6399bd96e86bdf193
Luian Diaz <luiandiaz09@gmail.com>
2019-06-05 12:32:33 -0400

    inicio_javascript

---
+++ javascript_101/0_script.js
@@ -0,0 +1,42 @@
+alert("Hola perros!!");
+/*Pedri al usuario que ingrese su edad*/
+/*soy un
+comentario
+multilinea*/
+
+
+//soy un comentario de 1 sola linea
+var nombre =prompt("Digite su nombre");
+var contraseña =prompt("Digite su contraseña");
+var tarjeta =prompt("Digite los codigos de su tarjeta de credito");
+var ciudad =prompt("Digite en que ciudad");
+var barrio=prompt("Digite el barrio mugroso")
+var direccion=prompt("Su direccion para tirarme para aya")
+console.log("nombre ");
+console.log("contraseña ");
+console.log("tarjeta ");
+console.log("ciudad ");
+
+document.write(`<h1>${nombre}</h1>`);
+document.write(`<p>${contraseña}</p>`);
+document.write(`<div>${barrio}</div>`);
+dacumento.write(`<div>${direccion}</div>`);
+document.write(tarjeta );
+document.write(ciudad );
+
+console.log(document.documentURI);
+console.log(document.body);
+console.log(document.head);
+console.log(document.height);
+console.log(document.URL);
+console.log(document.title);
+
+//Esta bariable no esta definida
+var pais;
+console.log(pais)
+
+var pais="Rep.Dom.";
+console.log(pais);
+
+//Canbiar el titulo
+document.title="Pendejo te lo cambie"
