67a6ef43c2ce58c56e916fce205f163b0ebd87bd
Luian Diaz <luiandiaz09@gmail.com>
2019-06-07 12:01:51 -0400

    parrafeando

---
+++ javascript_101/ejercicio.js
@@ -0,0 +1,8 @@
+var nombre=prompt("Digite su nombre");
+var edad=prompt("Digite su edad");
+
+
+document.write(`<p>Hola <u>${nombre}</u> tienes <u>${edad} anos</u></p>`);
+
+alert(`<p>Hola <u>${nombre}</u> tienes <u>${edad} anos</u></p>`);
+console.log(`<p>Hola <u>${nombre}</u> tienes <u>${edad} anos</u></p>`);
